22ca6759d723fe87a54c899b39e4e3066105025d
Shiva <shivaxwarrior@gmail.com>
2023-12-28 01:20:05 +0530

    refactor : file upload

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
+import { createServer } from "http";
 import express from "express";
+import { Server } from "socket.io";
 import dotenv from "dotenv";
 import cors from "cors";
-import { Server } from "socket.io";
+import multer from "multer";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -11,24 +14,23 @@ app.use(
   cors({
     origin: "*",
     methods: "GET,PUT,POST,DELETE",
-    allowedHeaders: "Content-Type",
+    allowedHeaders: "Content-Type, Recipient-Socket-Id, x-socket-id",
   })
 );
 
-const httpServer = app.listen(process.env.PORT || 8080, () =>
-  console.log(
-    `Server running on ${
-      process.env.TYPE === "DEVELOPMENT"
-        ? "http://localhost:8080"
-        : process.env.HOSTEDURL
-    }`
-  )
-);
+// Setup Multer for file uploads
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // 5MB limit, adjust as needed
+});
 
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
+    allowedHeaders: "Content-Type, Recipient-Socket-Id , x-socket-id",
   },
 });
 
@@ -47,8 +49,53 @@ io.on("connection", (socket) => {
     delete users[socket.id];
     io.emit("updateUsers", Object.values(users));
   });
+
+  // Handle file upload
+  socket.on("uploadFile", (fileData, recipientSocketId) => {
+    const recipientSocket = io.sockets.sockets.get(recipientSocketId);
+
+    if (recipientSocket) {
+      recipientSocket.emit("receiveFile", { sender: socket.id, fileData });
+    }
+  });
 });
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.post("/upload", upload.single("file"), (req, res) => {
+  try {
+    if (!req.file) {
+      throw new Error("No File Found");
+    }
+    console.log("File received:", req.file.originalname);
+    const fileData = req.file.buffer.toString("base64");
+    const recipientSocketId = req.headers["recipient-socket-id"];
+    console.log("Recipient socket ID:", recipientSocketId);
+    const recipientSocket = io.sockets.sockets.get(recipientSocketId);
+
+    if (recipientSocket) {
+      recipientSocket.emit("receiveFile", {
+        sender: req.headers["x-socket-id"],
+        fileData,
+      });
+      console.log("File uploaded successfully to", recipientSocketId);
+    } else {
+      console.error("Recipient socket not found:", recipientSocketId);
+    }
+
+    res.send("File uploaded successfully");
+  } catch (error) {
+    console.error("Error handling file upload:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
+
+const PORT = process.env.PORT || 8080;
+
+httpServer.listen(PORT, () =>
+  console.log(
+    `Server running on ${
+      process.env.TYPE === "DEVELOPMENT"
+        ? "http://localhost:8080"
+        : process.env.HOSTEDURL
+    }`
+  )
+);
--- frontend/src/pages/SendFile.jsx
+++ frontend/src/pages/SendFile.jsx
@@ -1,4 +1,3 @@
-// Import necessary libraries and components
 import React, { useEffect, useState } from "react";
 import { useNickname } from "../context/NickName";
 import Navbar from "../components/Navbar";
@@ -13,10 +12,10 @@ const SendFile = () => {
   const [connectedDevices, setConnectedDevices] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  const [selectedFileName, setSelectedFileName] = useState("");
+  const [uploadProgress, setUploadProgress] = useState(0);
   useEffect(() => {
     const socket = io("http://localhost:8080");
-
     socket.emit("setNickname", nickname);
     socket.on("updateUsers", (users) => {
       setConnectedDevices(users);
@@ -36,15 +35,44 @@ const SendFile = () => {
     return () => {
       socket.disconnect();
     };
-  }, [nickname]);
+  }, [nickname, loading]);
+
+  const handleFileUpload = async (event) => {
+    console.log("function invoked");
+    const file = event.target.files[0];
+    setSelectedFileName(event.target.files[0].name);
+    const formData = new FormData();
+    formData.append("file", file);
+
+    // Assuming connectedDevices.length > 1 and choosing the first device
+    const recipientSocketId = connectedDevices[1];
+    const socket = io("http://localhost:8080");
+    console.log("socket", socket);
+    // Send the file to the server with recipientSocketId as a custom header
+    try {
+      const response = await fetch("http://localhost:8080/upload", {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Recipient-Socket-Id": recipientSocketId,
+          "x-socket-id": socket.id,
+        },
+      });
+      console.log(response);
+      // Rest of the code remains unchanged
+    } catch (error) {
+      console.error("Error during file upload:", error.message);
+      setError(error.message);
+    }
+  };
 
   return (
     <div className="p-4 overflow-hidden lg:w-[90%] lg:m-auto">
       <Navbar />
       {loading ? (
-        <div className=" min-h-screen flex-col gap-3 tracking-wider font-semibold flex justify-center items-center">
+        <div className="min-h-screen flex-col gap-3 tracking-wider font-semibold flex justify-center items-center">
           <FadeLoader color="#fff" />
-          <h1 className=" uppercase">Establishing p2p</h1>
+          <h1 className="uppercase">Establishing p2p</h1>
           {error && (
             <div className="bg-red-500 text-white p-2 mt-2 rounded-md">
               {error}
@@ -52,7 +80,7 @@ const SendFile = () => {
           )}
         </div>
       ) : (
-        <div className=" flex flex-col justify-center">
+        <div className="flex flex-col justify-center">
           <Link
             to={"/home"}
             className="gap-2 lg:absolute lg:top-20 cursor-pointer lg:left-[28%] font-semibold text-lg  mt-5 z-20 flex items-center"
@@ -65,6 +93,11 @@ const SendFile = () => {
             <h1 className=" text-xl font-semibold tracking-wide">
               P2P FILE SHARING WITH ZEPHYR
             </h1>
+            {error && (
+              <div className="bg-red-500 absolute -top-0 z-50 font-semibold text-2xl w-full text-white p-2 mt-2 rounded-md">
+                {error}
+              </div>
+            )}
             <p className=" text-sm text-red-500/70 text-start font-semibold">
               Please make sure the devices are connected to the same wifi
               network
@@ -76,7 +109,7 @@ const SendFile = () => {
               className="w-[90%] mt-16 lg:mt-36 lg:w-80 opacity-70"
             />
 
-            <div className=" absolute mt-7 top-20 lg:w-1/2 lg:m-auto bg-white/5 border-2 border-white/10 shadow-white/15 p-4 overflow-y-scroll shadow-sm backdrop-blur-md w-[90%] h-96 rounded-lg">
+            <div className="absolute mt-7 top-20 lg:w-1/2 lg:m-auto bg-white/5 border-2 border-white/10 shadow-white/15 p-4 overflow-y-scroll shadow-sm backdrop-blur-lg w-[98%] h-96 rounded-lg">
               <div className="flex flex-wrap mt-16 flex-row justify-center items-center gap-7">
                 {connectedDevices.length < 1 ? (
                   <h1 className="text-white text-2xl font-semibold">
@@ -92,7 +125,7 @@ const SendFile = () => {
                         <div className="w-16 flex-col h-16 shadow-white/5 shadow-2xl flex justify-center items-center font-bold text-2xl bg-white text-black rounded-full">
                           {device.toUpperCase().split("")[0]}
                         </div>
-                        <h1 className=" truncate max-w-28">{device}</h1>
+                        <h1 className="truncate max-w-28">{device}</h1>
                       </div>
                     ))}
                   </>
@@ -102,16 +135,27 @@ const SendFile = () => {
             <button
               className={` ${
                 connectedDevices.length > 1 ? "bg-white" : "bg-white/30"
-              } text-black overflow-hidden flex gap-3 p-4 mt-[40%] lg:mt-10 font-semibold rounded-lg  `}
+              } text-black overflow-hidden flex gap-3 p-4 mt-28 lg:mt-10 font-semibold rounded-lg  `}
             >
               {connectedDevices.length > 1 && (
-                <input type="file" className="absolute left-0 opacity-0" />
+                <>
+                  <input
+                    onChange={handleFileUpload}
+                    type="file"
+                    className="absolute left-0 opacity-0"
+                  />
+                </>
               )}
               <ArrowUpToLine />
               Upload File
             </button>
 
-            <div className="border-2 mt-6 border-dashed w-full lg:w-[50%] lg:m-auto h-32 border-white/40 mb-10 lg:mb-20"></div>
+            <div className="border-2 min-h-full flex flex-col justify-center items-center overflow-y-scroll font-semibold mt-6 border-dashed w-full lg:w-[50%] lg:m-auto h-32 border-white/40 mb-10 lg:mb-20">
+              {uploadProgress > 0 && (
+                <span>{`${uploadProgress}% Uploaded`}</span>
+              )}
+              <span>{selectedFileName || "Choose File"}</span>
+            </div>
             <div className="flex flex-col gap-2 justify-center items-center">
               <h1 className="text-3xl opacity-50 tracking-widest uppercase font-bold">
                 Zephyr
